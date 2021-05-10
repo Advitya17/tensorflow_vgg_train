d0f2cd8f9836130f6558666109524888337d69f8
Advi17 <Advi17@node.gcnexps.orion-pg0.wisc.cloudlab.us>
2021-05-09 23:04:39 -0500

    initial feat script

--- extract_vgg16.py
+++ extract_vgg16.py
@@ -3,17 +3,48 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.models import Model
 import numpy as np
+import tensorflow as tf
+
+import pandas as pd
+import os
+
+feats = np.load('../GraphSAINT/data/flickr/feats.npy')
+feat_lookup = np.loadtxt('../GraphSAINT/data/BoW_int.dat')
+
+urls = pd.read_fwf('../GraphSAINT/data/NUS-WIDE-urls.txt')
+
+vgg_f = []
+
+counter = 0
+for img_f in feats:
+    for idx, f in enumerate(feat_lookup):
+        if np.array_equal(img_f, f):
+            counter += 1
+            # local move
+            link = urls.iloc[idx, 0].split()[0]
+            #os.system('mv ../' + '/'.join(link.split('/')[2:]) + ' .')
+            img_path = '../GraphSAINT/data/flickr_images/' + link.split('/')[-1]
+            img = image.load_img(img_path, target_size=(224, 224, 3))
+            x = image.img_to_array(img)
+            x = np.expand_dims(x, axis=0)
+            x = preprocess_input(x)
+
+            vgg_f.append(x)
+            print(counter)
+            break
+
+    # only for testing
+    if counter == 2:
+        break
+
+f = tf.concat(vgg_f, 0)
+print(f.shape)
 
 base_model = VGG16(weights='imagenet')
 model = Model(inputs=base_model.input, outputs=base_model.get_layer('block2_conv1').output)
 
-img_path = '../GraphSAINT/data/flickr_images/0464_245884868.jpg' #, '../GraphSAINT/data/flickr_images/0014_1993869440.jpg']
-img = image.load_img(img_path, target_size=(224, 224, 3))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
-print(x.shape)
-
-features = model.predict(x)
+features = model.predict(f)
+print(features.shape)
+features = tf.reshape(features, [counter, np.prod(features.shape)//counter])
 print(features.shape)
 
